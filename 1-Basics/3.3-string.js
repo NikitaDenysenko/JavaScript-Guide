a368d30511c3cb684d3581089e04743d716818d2
Nikita Denysenko <53399334+NikitaDenysenko@users.noreply.github.com>
2021-06-27 19:25:08 +0300

    added basic sting methods

--- 1-Basics/3.3-string.js
+++ 1-Basics/3.3-string.js
@@ -3,4 +3,25 @@
 const myName = "Joe";
 const age = 54;
 console.log("My name is" + " " + myName + "." + " " + "I am" + " " + age);
+//template literals
 console.log(`My name is ${myName}. I am ${age}`);
+
+let testString = "Hello, It's me, Mario!";
+let stringWithSapaces = "    the spaced text    ";
+let brokenString = "   Joe Doe the kInG oF Custle  ";
+
+console.log(testString[1], testString.charAt(1));
+console.table({
+    ".length": testString.length,
+    ".toLowerCase()": testString.toLowerCase(),
+    ".toUpperCase()": testString.toUpperCase(),
+    ".charAt()": testString.charAt(1),
+    ".indexOf()": testString.indexOf("o"),
+    ".trim()": stringWithSapaces.trim(),
+    ".startsWith()": testString.startsWith("Hello"),
+    ".includes()": testString.includes("Mario"),
+    ".slice()": testString.slice(0, 14), //does not inclueds last index
+});
+
+//chaining methods
+console.log(brokenString.trim().toLowerCase().startsWith("joe"));
