3b5f867efe12b60e7bbf93ef3b847ed338735def
Nikita Denysenko <53399334+NikitaDenysenko@users.noreply.github.com>
2021-06-27 19:48:23 +0300

    added basic array elements

--- 1-Basics/3.8-array.js
+++ 1-Basics/3.8-array.js
@@ -4,3 +4,25 @@ const randomDataInArray = ["str", 32, null, true, {}];
 console.log(randomDataInArray);
 randomDataInArray[4] = "used to be an object";
 console.log(randomDataInArray);
+
+let animalsArray = ["cats", "dogs", "racoons"];
+let birds = ["amazon bird", "parot", "peageon"];
+
+const demoConcat = (arr) => {
+    const input = animalsArray.toString();
+    const output = animalsArray.concat(arr).toString();
+    return `${input} => ${output}`;
+};
+
+console.table({
+    ".length": animalsArray.length,
+    ".concat()": demoConcat(birds),
+    ".reverse()": animalsArray.reverse(),
+    ".unshift()": animalsArray.unshift("lions"),
+    ".shift()": animalsArray.shift("lions"),
+    ".push()": animalsArray.push("elephants"),
+    ".pop()": animalsArray.pop("elephants"),
+    ".splice()": animalsArray.splice(2, 1),
+});
+
+console.log(animalsArray.toString());
