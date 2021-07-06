a80e8825ab8df97126b3a324df3d18593696da60
Nikita Denysenko <53399334+NikitaDenysenko@users.noreply.github.com>
2021-07-06 22:31:28 +0300

    added more tasks from codewars

--- 8-CodeWars/codewars.js
+++ 8-CodeWars/codewars.js
@@ -1,3 +1,4 @@
+//Array
 //Convert number to reversed array of digits (8)
 //https://www.codewars.com/kata/5583090cbe83f4fd8c000051/train/javascript
 
@@ -8,3 +9,29 @@ function digitize(num) {
 }
 console.log(digitize(num)); 
 */
+
+//Array
+//Count by X (8)
+//https://www.codewars.com/kata/5513795bd3fafb56c200049e/train/javascript
+
+/* 
+const countBy = (x, n) => {
+    let counter = 1;
+    const result = [];
+    while(counter <= n) {
+        result.push(x * counter)
+        counter++
+    }  
+    return result
+}
+*/
+
+//Array
+//Get the mean of an array 8
+//https://www.codewars.com/kata/563e320cee5dddcf77000158/train/javascript
+/*
+const getAverage = (marks) => {
+    return Math.floor(
+        marks.reduce((total, currentValue) => total + currentValue, 0) / marks.length
+    );
+} */
