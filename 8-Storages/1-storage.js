9b223d122dfc42c72fae5821618cd46011b1a2c5
Nikita Denysenko <53399334+NikitaDenysenko@users.noreply.github.com>
2021-07-07 21:54:30 +0300

    added basics for storages

---
+++ 8-Storages/1-storage.js
@@ -0,0 +1,8 @@
+/* localStorage, sessionStorage */
+sessionStorage.setItem("name", "Joe");
+
+localStorage.setItem("name", "Joe");
+localStorage.setItem("second name", "Doe");
+console.log(localStorage.getItem("name"));
+localStorage.removeItem("second name");
+localStorage.clear();
