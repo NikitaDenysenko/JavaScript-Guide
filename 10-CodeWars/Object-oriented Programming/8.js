1ec8d7c70c833369024f91d079398a8a9d9f6686
Nikita Denysenko <53399334+NikitaDenysenko@users.noreply.github.com>
2021-07-15 17:44:09 +0300

    practised OOP on codewars

---
+++ 10-CodeWars/Object-oriented Programming/8.js
@@ -0,0 +1,44 @@
+//https://www.codewars.com/kata/56be025f9347a066c7000e4f/train/javascript
+//Improving Math.round(x)
+/*
+Math.roundTo = function (number, precision) {
+    return Number(number.toFixed(precision));
+};
+*/
+
+//https://www.codewars.com/kata/54fe05c4762e2e3047000add/train/javascript
+//Object Oriented Piracy
+/*
+class Ship {
+    constructor(draft, crew) {
+        this.draft = draft;
+        this.crew = crew;
+    }
+
+    isWorthIt() {
+        return this.draft - this.crew * 1.5 > 20;
+    }
+}
+*/
+
+//https://www.codewars.com/kata/55a14aa4817efe41c20000bc/train/javascript
+//Classy Extentions
+/* 
+class Cat extends Animal {
+    constructor(name) {
+        super()
+        this.name = name
+    }
+    speak() {
+        return `${this.name} meows.`
+    }
+}
+*/
+
+//Is the string uppercase?
+//https://www.codewars.com/kata/56cd44e1aa4ac7879200010b/train/javascript
+/*
+String.prototype.isUpperCase = function () {
+    return this.valueOf() === this.valueOf().toUpperCase()
+};
+*/
