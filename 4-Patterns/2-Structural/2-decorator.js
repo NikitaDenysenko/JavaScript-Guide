12060d58d9e051852950e50ce340820c90254614
Nikita Denysenko <53399334+NikitaDenysenko@users.noreply.github.com>
2021-09-13 13:47:50 +0300

    added decorator

---
+++ 4-Patterns/2-Structural/2-decorator.js
@@ -0,0 +1,20 @@
+class Server {
+    constructor(ip, port) {
+        this.ip = ip
+        this.port = port
+    }
+   get url() {
+       return `https://${this.ip}:${this.port}`
+   } 
+}
+//decorator
+function aws(server) {
+    server.isAWS = true
+    server.awsInfo = function () {
+        return server.url
+    }
+
+    return server
+}
+
+const s1 = aws(new Server('12.34.56.78', 8080))
